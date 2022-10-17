4c6e2aade38577e2fb595a8ffd2731d3c876ce79
Olvinou <103410734+Olvinou@users.noreply.github.com>
2022-10-17 10:12:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import pickle
 
 app = FastAPI()
 
-df = pd.read_csv('test_featureengineering.csv').iloc[:,1:]
-
 model = pickle.load(open("best_model_custom.pkl","rb"))
 
 @app.get("/{client}")
 def get_score(client: int):
-
-    return float(model.predict_proba(df[df['SK_ID_CURR'] == int(client)].iloc[:,1:])[:,1])
+    df = pd.read_csv('test_featureengineering.csv').iloc[:,1:]
+    df = df[df['SK_ID_CURR'] == int(client)].iloc[:,1:]
+    return float(model.predict_proba(df)[:,1])
