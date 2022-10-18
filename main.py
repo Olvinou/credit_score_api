de8caef998b1b266181cb6b3afad9e5446c27aa2
Olvinou <103410734+Olvinou@users.noreply.github.com>
2022-10-18 14:48:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,30 @@
 from fastapi import FastAPI
 import pandas as pd
 import pickle
-
+import shap
 
 app = FastAPI()
 
 model = pickle.load(open("best_model_custom.pkl","rb"))
 
-@app.get("/{client}")
+df = pd.read_csv('test_featureengineering.csv').iloc[:,1:]
+
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(df.iloc[:,1:])
+
+@app.get("/{client}/score")
 def get_score(client: int):
-    df = pd.read_csv('test_featureengineering.csv').iloc[:,1:]
-    df = df[df['SK_ID_CURR'] == int(client)].iloc[:,1:]
-    return float(model.predict_proba(df)[:,1])
+   
+    df_score = df[df['SK_ID_CURR'] == int(client)].iloc[:,1:]
+    return float(model.predict_proba(df_score)[:,1])
+
+@app.get("/{client}/shap_value")
+def get_shap_value(client: int):
+    index = df.index[df['SK_ID_CURR'] == client].tolist()[0]
+    
+    return shap_values[0][index].tolist()
+
+@app.get("/{client}/expected_value")
+def get_shap_value(client: int):
+    
+    return explainer.expected_value[0]
